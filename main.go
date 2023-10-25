92cbf0f27009119a0525667f85766f0a5106dbd8
Brandon Tripp <brandongtripp@gmail.com>
2023-10-24 19:49:45 -0600

    Add aws-lambda-go package to help with lambdas in Go, initial sample lambda route

--- main.go
+++ main.go
@@ -1,23 +1,40 @@
 package main
 
 import (
-	"fmt"
+	// "fmt"
 	"log"
-	"net/http"
+	// "net/http"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func handleHelloTest(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("Hello from the Go server!!"))
+// func handleHelloTest(w http.ResponseWriter, r *http.Request) {
+//     w.Write([]byte("Hello from the Go server!!"))
+// }
+
+func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+    log.Println("Hello world")
+
+    resp := events.APIGatewayProxyResponse{
+        StatusCode: 200, 
+    }
+
+    return resp, nil 
 } 
+
 
 
 
 func main() {
-    server := http.NewServeMux()
-    server.HandleFunc("/", handleHelloTest)
+    lambda.Start(handler)
+    // server := http.NewServeMux()
+    // server.HandleFunc("/", handleHelloTest)
 
-    fmt.Printf("Serving on PORT 3001")
-    if err := http.ListenAndServe(":3001", server); err != nil {
-        log.Fatal(err)
-    } 
+    // fmt.Printf("Serving on PORT 3001")
+    // if err := http.ListenAndServe(":3001", server); err != nil {
+    //     log.Fatal(err)
+    // } 
 } 
+
+
