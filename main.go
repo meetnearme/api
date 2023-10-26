9dde8ffb8883bff84421f8bae5cc0ccbe739d3a5
Brandon Tripp <brandongtripp@gmail.com>
2023-10-25 20:51:05 -0600

    add sample json event and main handler

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	// "fmt"
+	"context"
+	"fmt"
 	"log"
+
 	// "net/http"
 
-	"github.com/aws/aws-lambda-go/events"
+	// "github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
@@ -13,16 +16,24 @@ import (
 //     w.Write([]byte("Hello from the Go server!!"))
 // }
 
-func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-    log.Println("Hello world")
+// func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+//     fmt.Println("Hello World")
+//     log.Println("Hello world")
 
-    resp := events.APIGatewayProxyResponse{
-        StatusCode: 200, 
-    }
+//     resp := events.APIGatewayProxyResponse{
+//         StatusCode: 200,
+//     }
 
-    return resp, nil 
-} 
+//     return resp, nil
+// }
 
+func handler(ctx context.Context, event Event) (string, error) {
+        log.Print("value1 = ", event["key1"] )
+        log.Print("value2 = ", event["key2"] )
+        log.Print("value3 = ", event["key3"] )
+
+        return fmt.Sprintf("Hello World"), nil
+}
 
 
 
