71559b21efad4eeb7c87525c204fd1e5d45eea04
Brandon Tripp <brandongtripp@gmail.com>
2023-10-26 14:31:36 -0600

    Specify port 3001 to avoid conflict, set lambdas to warm containers EAGER

--- sam_cli_entrypoint.sh
+++ sam_cli_entrypoint.sh
@@ -1,1 +1,1 @@
-sam local start-api --host=0.0.0.0 --container-host=host.docker.internal --docker-network aws_backend
+sam local start-api -p 3001 --host=0.0.0.0 --container-host=host.docker.internal --docker-network aws_backend --warm-containers EAGER
