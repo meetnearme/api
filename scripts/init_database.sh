423ace243913d1095d85b822a533c709cdab3f75
Brandon Tripp <brandongtripp@gmail.com>
2023-11-04 12:40:43 -0600

    Echos for stages of db init

--- scripts/init_database.sh
+++ scripts/init_database.sh
@@ -8,10 +8,12 @@ echo "start sleep"
 sleep 5
 echo "end sleep"
 
-aws dynamodb create-table --cli-input-json file://internal/database/db_seeds/create_event_table.json --endpoint-url http://dynamodb-local:8000 --region us-east-1
+aws dynamodb create-table --cli-input-json file://internal/database/db_seeds/create_user_table.json --endpoint-url http://dynamodb-local:8000 --region us-east-1
+echo "end create table"
 aws dynamodb list-tables --endpoint-url http://dynamodb-local:8000 --region us-east-1
-
+echo "end list tables"
 aws dynamodb batch-write-item --request-items file://internal/database/db_seeds/seed_user_records.json --endpoint-url http://dynamodb-local:8000 --region us-east-1
+echo "end batch write"
 
 echo "database seed complete"
 if [ $1 == "--forever" ]
