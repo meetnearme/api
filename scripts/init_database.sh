eddbaeea66504e42cd92109fe28baeb0fbdc827a
Brandon Tripp <brandongtripp@gmail.com>
2023-12-07 18:01:20 -0700

    Remove dummy dynamodb data for users and replace with mock event data

--- scripts/init_database.sh
+++ scripts/init_database.sh
@@ -8,12 +8,15 @@ echo "start sleep"
 sleep 5
 echo "end sleep"
 
-aws dynamodb create-table --cli-input-json file://internal/database/db_seeds/create_user_table.json --endpoint-url http://dynamodb-local:8000 --region us-east-1
+# create and seed event information 
+aws dynamodb create-table --cli-input-json file://internal/database/db_seeds/create_event_table.json --endpoint-url http://dynamodb-local:8000 --region us-east-1
 echo "end create table"
 aws dynamodb list-tables --endpoint-url http://dynamodb-local:8000 --region us-east-1
 echo "end list tables"
-aws dynamodb batch-write-item --request-items file://internal/database/db_seeds/seed_user_records.json --endpoint-url http://dynamodb-local:8000 --region us-east-1
+aws dynamodb batch-write-item --request-items file://internal/database/db_seeds/seed_event_records.json --endpoint-url http://dynamodb-local:8000 --region us-east-1
 echo "end batch write"
+aws dynamodb scan --table-name Events --endpoint-url http://dynamodb-local:8000 --region us-east-1
+echo "end scan event table"
 
 echo "database seed complete"
 if [ $1 == "--forever" ]
