4cf9422d42c62af427c51dd67cb6e2b0d007a1c9
Brandon Tripp <brandongtripp@gmail.com>
2024-01-18 08:36:04 -0700

    Initialization of internal sst project for migration, boilerplate and move of main.go and routes into project

---
+++ rest-api-go/functions/lambda/main.go
@@ -0,0 +1,26 @@
+package main
+
+import ( 
+
+	// "github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+
+// func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+//     fmt.Println("Hello World")
+//     log.Println("Hello world")
+
+//     resp := events.APIGatewayProxyResponse{
+//         StatusCode: 200,
+//         Body: "I am returning from the server",
+//     }
+
+//     return resp, nil
+// }
+
+func main() {
+    lambda.Start(router)
+} 
+
+
