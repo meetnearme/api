96b43d23fa8c8988e0743724674031838a1b8faf
Brian Feister <brian.feister@gmail.com>
2024-02-01 14:30:53 -0700

    fix submodule reference (needs to declare `package views` in `functions/lambda/views` and also import `views` in `main`, then reference `views.Home`). Capitalization matters here, if lowercase, it's not exported to the module

--- functions/lambda/main.go
+++ functions/lambda/main.go
@@ -5,16 +5,13 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
 
-	"github.com/a-h/templ"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/go-playground/validator/v10"
-	// import relative module for views handled by templ
-    "github.com/meetnearme/api/views"
+	"github.com/meetnearme/api/functions/lambda/views"
 )
 
 var validate *validator.Validate = validator.New()
@@ -28,57 +25,10 @@ type CreateEvent struct {
     Country string  `json:"country" validate:"required"`
 }
 
-func main() {
-    lambda.Start(Router)
-}
-
-// func hello(name string) templ.Component {
-// 	return templ.FromGoHTML(func(ctx context.Context, name string) (interface{}, error) {
-// 		return templ.ToGoHTML(ctx, fmt.Sprintf("Hello, %s!", name))
-// 	})
-// }
-
-func hello(name string) templ.Component {
-	return templ.ComponentFunc(func(ctx context.Context, templ_7745c5c3_W io.Writer) (templ_7745c5c3_Err error) {
-		templ_7745c5c3_Buffer, templ_7745c5c3_IsBuffer := templ_7745c5c3_W.(*bytes.Buffer)
-		if !templ_7745c5c3_IsBuffer {
-			templ_7745c5c3_Buffer = templ.GetBuffer()
-			defer templ.ReleaseBuffer(templ_7745c5c3_Buffer)
-		}
-		ctx = templ.InitializeContext(ctx)
-		templ_7745c5c3_Var1 := templ.GetChildren(ctx)
-		if templ_7745c5c3_Var1 == nil {
-			templ_7745c5c3_Var1 = templ.NopComponent
-		}
-		ctx = templ.ClearChildren(ctx)
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<div>")
-		if templ_7745c5c3_Err != nil {
-			return templ_7745c5c3_Err
-		}
-		var templ_7745c5c3_Var2 string
-		templ_7745c5c3_Var2, templ_7745c5c3_Err = templ.JoinStringErrs(name)
-		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `functions/lambda/views/hello.templ`, Line: 3, Col: 12}
-		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var2))
-		if templ_7745c5c3_Err != nil {
-			return templ_7745c5c3_Err
-		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</div>")
-		if templ_7745c5c3_Err != nil {
-			return templ_7745c5c3_Err
-		}
-		if !templ_7745c5c3_IsBuffer {
-			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteTo(templ_7745c5c3_W)
-		}
-		return templ_7745c5c3_Err
-	})
-}
-
 func Router(ctx context.Context, req events.APIGatewayV2HTTPRequest) (events.APIGatewayV2HTTPResponse, error) {
     switch req.RequestContext.HTTP.Method {
     case "GET":
-        component := Hello("World")
+        component := views.Hello("World")
         var buf bytes.Buffer
         err := component.Render(ctx, &buf)
         if err != nil {
@@ -167,3 +117,6 @@ func serverError(err error) (events.APIGatewayV2HTTPResponse, error) {
 	}, nil
 }
 
+func main() {
+    lambda.Start(Router)
+}
