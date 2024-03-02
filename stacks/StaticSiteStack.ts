b2c219063c500dae8ff4d279237f754d7c333d42
Brian Feister <brian.feister@gmail.com>
2024-03-02 08:37:04 -0700

    Add `dev: deploy: true` for debugging, output StaticEndpoint / URL and the S3 bucket CloudFront URL / domain... Working for local deployments, might be a permissions issue with the deployment user... testing

--- stacks/StaticSiteStack.ts
+++ stacks/StaticSiteStack.ts
@@ -1,18 +1,23 @@
 import { Api, StaticSite, use } from 'sst/constructs';
 import { ApiStack } from './ApiStack';
-const util = require('node:util');
 
 // TODO: pass `api` created from that stack in to get `api.url` here for `alternateNames`
 export function StaticSiteStack({ stack }: StackContext) {
   const { api } = use(ApiStack);
-  console.log('~api.url', util.inspect(api.url));
   const staticSite = new StaticSite(stack, 'frontend', {
     path: 'static',
+    dev: {
+      deploy: true,
+    },
     // TODO: figure out a domain name with associated cert
     // customDomain: {
     //   alternateNames: [api.url],
     //   domainName: 'domain.com',
     // },
+  });
+  stack.addOutputs({
+    StaticEndpoint: staticSite.url,
+    BucketDomainName: staticSite.bucket.bucketDomainName,
   });
 
   return { staticSite };
