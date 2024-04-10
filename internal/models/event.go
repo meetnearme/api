4c5d5d8d60d932861746f16d50a1b137fd203fdb
Brandon Tripp <brandongtripp@gmail.com>
2024-04-10 15:06:37 -0600

    Go Event struct to model data for JS and dynamodb

---
+++ internal/models/event.go
@@ -0,0 +1,13 @@
+package models
+
+type Event struct {
+    ID string `json:"id" dynamodbav:"id"`
+    StartTime string `json:"startTime" dynamodbav:"start_time" validator:"required"`
+    Latitude float64 `json:"latitude" dynamodbav:"latitude" validator:"required"`
+    Longitude float64 `json:"longitude" dynamodbav:"longitude" validator:"required"`
+    ZOrderIndex string `json:"zOrderIndex" dynamodbav:"z_order_index" validator:"required"`
+    Title string `json:"title" dynamodbav:"title" validator:"required"`
+    Description string `json:"description" dynamodbav:"description" validator:"required"`
+    Location string `json:"location" dynamodbav:"location" validator:"required"`
+    URL string `json:"url" dynamodbav:"url" validator:"url"`
+} 
