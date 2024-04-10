406e54e385c5913e4b5ea2681bad55f4bb8cca3f
Brandon Tripp <brandongtripp@gmail.com>
2024-04-10 15:09:19 -0600

    Update Event struct field for ZOrderIndex to []byte

--- internal/models/event.go
+++ internal/models/event.go
@@ -1,11 +1,13 @@
 package models
 
+import "github.com/go-playground/validator/v10"
+
 type Event struct {
     ID string `json:"id" dynamodbav:"id"`
     StartTime string `json:"startTime" dynamodbav:"start_time" validator:"required"`
     Latitude float64 `json:"latitude" dynamodbav:"latitude" validator:"required"`
     Longitude float64 `json:"longitude" dynamodbav:"longitude" validator:"required"`
-    ZOrderIndex string `json:"zOrderIndex" dynamodbav:"z_order_index" validator:"required"`
+    ZOrderIndex []byte `json:"zOrderIndex" dynamodbav:"z_order_index" validator:"required"`
     Title string `json:"title" dynamodbav:"title" validator:"required"`
     Description string `json:"description" dynamodbav:"description" validator:"required"`
     Location string `json:"location" dynamodbav:"location" validator:"required"`
