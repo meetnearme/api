342c2a882cc4e492016a3b3c53708c6f02994d42
Brandon Tripp <brandongtripp@gmail.com>
2024-07-23 17:28:06 -0600

    fix(go_lambda_test_coverage): write the implementation of each of the http.go functions with a func(w http.ResponseWriter, r *http.Request) returned function

--- functions/gateway/transport/http.go
+++ functions/gateway/transport/http.go
@@ -7,36 +7,42 @@ import (
 
 // NOTE: `err` is passed in and logged if status is 400 or greater, but msg
 func SendHtmlRes(w http.ResponseWriter, body []byte, status int, err error) http.HandlerFunc {
-	msg := string(body)
-	if (status >= 400) {
-		msg = "ERR: "+msg
-		log.Println(msg+ " || Internal error msg: "+err.Error())
-	}
-	w.Header().Set("Content-Type", "text/html")
-	w.WriteHeader(status)
-	w.Write(body)
-
-	return http.HandlerFunc(nil)
+    return func(w http.ResponseWriter, r *http.Request) {
+        log.Println("In SendHtmlRes")
+        defer log.Println("About to leave Send HTML")
+        msg := string(body)
+        if (status >= 400) {
+            log.Println("Error occured:", err)
+            msg = "ERR: "+msg
+            log.Println(msg+ " || Internal error msg: "+err.Error())
+        }
+        w.Header().Set("Content-Type", "text/html")
+        w.WriteHeader(status)
+        _, writeErr := w.Write(body)
+        if writeErr != nil {
+            log.Println("Error writing response:", writeErr)
+        }
+    }
 }
 
 func SendHtmlError(w http.ResponseWriter, body []byte, status int) http.HandlerFunc {
-	w.Header().Set("Content-Type", "text/html")
-	w.WriteHeader(status)
-	w.Write(body)
-
-	return http.HandlerFunc(nil)
+    return func(w http.ResponseWriter, r *http.Request) {
+        w.Header().Set("Content-Type", "text/html")
+        w.WriteHeader(status)
+        w.Write(body)
+    }
 }
 
 
 func SendServerRes(w http.ResponseWriter, body []byte, status int, err error) http.HandlerFunc {
-	msg := string(body)
-	if (status >= 400) {
-		msg = "ERR: "+msg
-		log.Println(msg)
-	}
+    return func(w http.ResponseWriter, r *http.Request) {
+        msg := string(body)
+        if (status >= 400) {
+            msg = "ERR: "+msg
+            log.Println(msg)
+        }
 
-	w.WriteHeader(status)
-	w.Write([]byte(msg))
-
-	return http.HandlerFunc(nil)
+        w.WriteHeader(status)
+        w.Write([]byte(msg))
+    }
 }
