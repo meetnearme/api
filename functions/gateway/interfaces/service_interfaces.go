21874e2f8875a58088a4fdf1df0854f44d0de06e
Brandon Tripp <brandongtripp@gmail.com>
2024-07-26 15:23:32 -0600

    Feat(go_lambda_test_coverage): new interfaces for seshu service. Partially implemented for future testing.
    TODO: testing strategy for the seshu service

---
+++ functions/gateway/interfaces/service_interfaces.go
@@ -0,0 +1,20 @@
+package interfaces
+
+import (
+	"context"
+	"errors"
+
+	"github.com/meetnearme/api/functions/gateway/types"
+)
+
+type GeoServiceInterface interface {
+    GetGeo(location, baseUrl string) (string, string, string, error)
+}
+
+type SeshuServiceInterface interface {
+    GetSeshuSession(ctx context.Context, db types.DynamoDBAPI, seshuPayload types.SeshuSession) (*types.SeshuSession, error)
+    InsertSeshuSession(ctx context.Context, db types.DynamoDBAPI, seshuPayload types.SeshuSessionInput) (*types.SeshuSessionInsert, error)
+    UpdateSeshuSession(ctx context.Context, db types.DynamoDBAPI, seshuPayload types.SeshuSessionUpdate) (*types.SeshuSessionUpdate, error)
+}
+
+var ErrInvalidLocation = errors.New("location is not valid")
