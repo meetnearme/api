648c31241bc654e8b3445ec7459346668ca5c5e1
Brandon Tripp <brandongtripp@gmail.com>
2024-07-29 20:07:14 -0600

    Refactor(go_lambda_test_coverage): refactoring of the data_handlers to be more idiomatic and make testing easier

--- functions/gateway/handlers/data_handlers.go
+++ functions/gateway/handlers/data_handlers.go
@@ -1,7 +1,6 @@
 package handlers
 
 import (
-	"context"
 	"encoding/json"
 	"io"
 	"log"
@@ -10,70 +9,60 @@ import (
 	"github.com/go-playground/validator"
 	"github.com/meetnearme/api/functions/gateway/services"
 	"github.com/meetnearme/api/functions/gateway/transport"
-    "github.com/meetnearme/api/functions/gateway/types"
 )
 
 var validate *validator.Validate = validator.New()
 
-// EventServiceInterface defines the methods we need from the services package
-type EventServiceInterface interface {
-    InsertEvent(ctx context.Context, db types.DynamoDBAPI, createEvent services.EventInsert) (*services.EventSelect, error)
+
+type EventHandler struct {
+    EventService services.EventServiceInterface
 }
 
-// RealEventService is a wrapper for the actual services package
-type RealEventService struct{}
-
-func (s *RealEventService) InsertEvent(ctx context.Context, db types.DynamoDBAPI, createEvent services.EventInsert) (*services.EventSelect, error) {
-    return services.InsertEvent(ctx, db, createEvent)
+func NewEventHandler(eventService services.EventServiceInterface) *EventHandler {
+    return &EventHandler{EventService: eventService}
 }
 
-// Wrapper function to satisfy the lambdaHandlerFunc type 
-func CreateEvent(w http.ResponseWriter, r *http.Request) http.HandlerFunc {
-    return func(w http.ResponseWriter, r *http.Request) {
-        db := Db
-        if db == nil {
-            db = transport.GetDB()
-        }
-        createEventHandler(r.Context(), w, r, &RealEventService{}, db)
+func (h *EventHandler) CreateEvent(w http.ResponseWriter, r *http.Request) {
+    var createEvent services.EventInsert
+    body, err := io.ReadAll(r.Body)
+    if err != nil {
+        transport.SendServerRes(w, []byte("Failed to read request body: "+err.Error()), http.StatusBadRequest, err)
+        return
     }
+
+    err = json.Unmarshal(body, &createEvent)
+    if err != nil {
+        transport.SendServerRes(w, []byte("Invalid JSON payload: "+err.Error()), http.StatusUnprocessableEntity, err)
+        return
+    }
+
+    err = validate.Struct(&createEvent)
+    if err != nil {
+        transport.SendServerRes(w, []byte("Invalid body: "+err.Error()), http.StatusBadRequest, err)
+        return 
+    }
+
+    db := transport.GetDB()
+    res, err := h.EventService.InsertEvent(r.Context(), db, createEvent)
+    if err != nil {
+        transport.SendServerRes(w, []byte("Failed to add event: "+err.Error()), http.StatusInternalServerError, err)
+        return
+    }
+
+    json, err := json.Marshal(res)
+    if err != nil {
+        transport.SendServerRes(w, []byte("Error marshaling JSON"), http.StatusInternalServerError, err)
+        return
+    }
+
+    log.Printf("Inserted new item: %+v", res)
+    transport.SendServerRes(w, json, http.StatusCreated, nil)
 }
 
-func createEventHandler(ctx context.Context, w  http.ResponseWriter, r *http.Request, eventService EventServiceInterface, db types.DynamoDBAPI) {
-	var createEvent services.EventInsert
-	body, err := io.ReadAll(r.Body)
-	if err != nil {
-		transport.SendServerRes(w, []byte("Failed to read request body: "+err.Error()), http.StatusBadRequest, err)
-        return
-	}
-
-	err = json.Unmarshal(body, &createEvent)
-	if err != nil {
-		transport.SendServerRes(w, []byte("Invalid JSON payload: "+err.Error()), http.StatusUnprocessableEntity, err)
-        return
-	}
-
-	err = validate.Struct(&createEvent)
-	if err != nil {
-		transport.SendServerRes(w, []byte("Invalid body: "+err.Error()), http.StatusBadRequest, err)
-        return 
-	}
-
-	res, err := eventService.InsertEvent(ctx, db, createEvent)
-	if err != nil {
-		transport.SendServerRes(w, []byte("Failed to add event: "+err.Error()), http.StatusInternalServerError, err)
-        return
-	}
-
-	json, err := json.Marshal(res)
-
-	if err != nil {
-		transport.SendServerRes(w, []byte("Error marshaling JSON"), http.StatusInternalServerError, err)
-        return
-	}
-
-	// TODO: consider log levels / log volume
-	log.Printf("Inserted new item: %+v", res)
-
-	// TODO: Replace JSON response with htmx template with event data
-	transport.SendServerRes(w, []byte(string(json)), http.StatusCreated, nil)
+func CreateEventHandler(w http.ResponseWriter, r *http.Request) http.HandlerFunc {
+    eventService := services.NewEventService()
+    handler := NewEventHandler(eventService)
+    return func(w http.ResponseWriter, r *http.Request) {
+        handler.CreateEvent(w, r)
+    }
 }
