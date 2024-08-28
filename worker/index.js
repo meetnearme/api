fd6991789f0250616757c7a4277c78ab9985b730
Brian Feister <brian.feister@gmail.com>
2024-08-28 10:22:33 -0600

    adhere to cf worker requirement for return type `Response`

--- worker/index.js
+++ worker/index.js
@@ -1,24 +1,41 @@
-addEventListener('fetch', (event) => {
-  event.respondWith(handleRequest(event.request, event));
-});
+export default {
+  async fetch(req, env) {
+    const url = new URL(req.url);
+    const hostname = url.hostname;
+    const parts = hostname.split('.');
+    const subdomain = parts.slice(0, -2).join('.');
 
-async function handleRequest(request, event) {
-  const url = new URL(request.url);
-  const hostname = url.hostname;
-  const parts = hostname.split('.');
-  const subdomain = parts.slice(0, -2).join('.');
+    console.log('req.cf: ', JSON.stringify(req.cf));
 
-  let subdomainValue = null;
+    let subdomainValue = null;
 
-  if (subdomain) {
-    subdomainValue = await event.env.MNM_SUBDOMAIN_KV_NAMESPACE.get(subdomain);
+    // Clone the original request
+    let newRequest = new Request(req);
 
-    // Add custom headers to the original request
-    if (subdomainValue) {
-      request.headers.set('X-Mnm-Subdomain-Value', subdomainValue);
+    if (subdomain) {
+      subdomainValue = await env.MNM_SUBDOMAIN_KV_NAMESPACE.get(subdomain);
+
+      // Add custom headers
+      if (subdomainValue) {
+        newRequest.headers.set('X-Mnm-Subdomain-Value', subdomainValue);
+      }
     }
-  }
 
-  // Pass through the modified request
-  return fetch(request);
-}
+    const response = await fetch(newRequest);
+
+    // Create a new response with the same body, status, and headers
+    const newResponse = new Response(response.body, {
+      status: response.status,
+      statusText: response.statusText,
+      headers: response.headers,
+    });
+
+    // Add the custom header to the new response if it exists
+    if (subdomainValue) {
+      newResponse.headers.set('X-Mnm-Subdomain-Value', subdomainValue);
+    }
+
+    // Return the new Response
+    return newResponse;
+  },
+};
