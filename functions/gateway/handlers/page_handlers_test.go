89bfccd4cf738fd11568aae02e32424b452ad079
Brian Feister <brian.feister@gmail.com>
2024-09-09 08:26:14 -0600

    restore event details page test, add better coverage to ensure output contains event input from mock server

--- functions/gateway/handlers/page_handlers_test.go
+++ functions/gateway/handlers/page_handlers_test.go
@@ -2,11 +2,17 @@ package handlers
 
 import (
 	"context"
+	"encoding/json"
+	"log"
+	"net"
 	"net/http"
 	"net/http/httptest"
+	"os"
+	"strings"
 	"testing"
 
 	"github.com/aws/aws-lambda-go/events"
+	"github.com/gorilla/mux"
 	"github.com/meetnearme/api/functions/gateway/helpers"
 )
 
@@ -136,38 +142,128 @@ func TestGetMapEmbedPage(t *testing.T) {
 
 // TODO: DO NOT MERGE commented out for reduced testing log noise
 
-// func TestGetEventDetailsPage(t *testing.T) {
-// 	const eventID = "123"
-// 	req, err := http.NewRequest("GET", "/events/" + eventID, nil)
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
+func TestGetEventDetailsPage(t *testing.T) {
 
-// 	// Set up context with APIGatewayV2HTTPRequest
-// 	ctx := context.WithValue(req.Context(), helpers.ApiGwV2ReqKey, events.APIGatewayV2HTTPRequest{
-//     PathParameters: map[string]string{
-//         helpers.EVENT_ID_KEY: eventID,
-//     },
-// 	})
-// 	req = req.WithContext(ctx)
+	// Save original environment variables
+	originalMarqoApiKey := os.Getenv("MARQO_API_KEY")
+	originalMarqoEndpoint := os.Getenv("MARQO_API_BASE_URL")
 
-// 	// Set up router to extract variables
-// 	router := mux.NewRouter()
-// 	router.HandleFunc("/events/{" + helpers.EVENT_ID_KEY + "}", func(w http.ResponseWriter, r *http.Request) {
-// 		GetEventDetailsPage(w, r).ServeHTTP(w, r)
-// 	})
+	// Set test environment variables
+	testMarqoApiKey := "test-marqo-api-key"
+	testMarqoEndpoint := helpers.MOCK_MARQO_URL
+	os.Setenv("MARQO_API_KEY", testMarqoApiKey)
+	os.Setenv("MARQO_API_BASE_URL", testMarqoEndpoint)
 
-// 	rr := httptest.NewRecorder()
-// 	router.ServeHTTP(rr, req)
+	// Defer resetting environment variables
+	defer func() {
+		os.Setenv("MARQO_API_KEY", originalMarqoApiKey)
+		os.Setenv("MARQO_API_BASE_URL", originalMarqoEndpoint)
+	}()
 
-// 	if status := rr.Code; status != http.StatusOK {
-// 		t.Errorf("Handler returned wrong status code: got %v want %v", status, http.StatusOK)
-// 	}
+	// Create a mock HTTP server for Marqo
+	mockMarqoServer := httptest.NewUnstartedServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Check the authorization header
+		// authHeader := r.Header.Get("x-api-key")
+    //     // we do nothing here because the underlying implementation of marqo go
+    //     // library implements `WithMarqoCloudAuth` as an option expected in our
+    //     // implementation, so omitting the auth header will result a lib failure
+		// if authHeader == "" {
+    //         return
+		// }
 
-// 	if rr.Body.String() == "" {
-// 		t.Errorf("Handler returned empty body")
-// 	}
+//  TODO: here is search schema
+
+// type SearchResponse struct {
+// 	// Hits is the list of hits
+// 	Hits []map[string]interface{} `json:"hits"`
+// 	// Limit is the number of results to return (default: 20)
+// 	Limit int `json:"limit"`
+// 	// Offset is the number of results to skip (default: 0)
+// 	Offset int `json:"offset"`
+// 	// ProcessingTimeMS is the processing time in milliseconds
+// 	ProcessingTimeMS float64 `json:"processingTimeMs"`
+// 	// Query is the query string
+// 	Query string `json:"query"`
 // }
+
+log.Printf("\n\n\n mock server hit... req= >>>> %+v", r)
+		// Mock the response
+
+		response := map[string]interface{}{
+			"results": []map[string]interface{}{
+				{
+					"id":          "123",
+					"eventOwners": []interface{}{"789"},
+					"name":        "Test Event",
+					"description": "This is a test event",
+					// Add other fields as needed
+				},
+			},
+		}
+		responseBytes, err := json.Marshal(response)
+
+
+
+		if err != nil {
+			http.Error(w, "Failed to marshal response", http.StatusInternalServerError)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+		w.Write(responseBytes)
+	}))
+
+	// Set the mock Marqo server URL
+	mockMarqoServer.Listener.Close()
+	var err error
+	mockMarqoServer.Listener, err = net.Listen("tcp", testMarqoEndpoint[len("http://"):])
+	if err != nil {
+		t.Fatalf("Failed to start mock Marqo server: %v", err)
+	} else {
+        t.Log("Started mock Marqo server")
+    }
+	mockMarqoServer.Start()
+	defer mockMarqoServer.Close()
+
+
+	const eventID = "123"
+	req, err := http.NewRequest("GET", "/events/" + eventID, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Set up context with APIGatewayV2HTTPRequest
+	ctx := context.WithValue(req.Context(), helpers.ApiGwV2ReqKey, events.APIGatewayV2HTTPRequest{
+    PathParameters: map[string]string{
+        helpers.EVENT_ID_KEY: eventID,
+    },
+	})
+	req = req.WithContext(ctx)
+
+	// Set up router to extract variables
+	router := mux.NewRouter()
+	router.HandleFunc("/events/{" + helpers.EVENT_ID_KEY + "}", func(w http.ResponseWriter, r *http.Request) {
+		GetEventDetailsPage(w, r).ServeHTTP(w, r)
+	})
+
+	rr := httptest.NewRecorder()
+	router.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("Handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+
+	if rr.Body.String() == "" {
+		t.Errorf("Handler returned empty body")
+	}
+
+	if !strings.Contains(rr.Body.String(), ">Test Event") {
+		t.Errorf("Event title is missing from the page")
+	}
+
+	if !strings.Contains(rr.Body.String(), ">This is a test event") {
+		t.Errorf("Event description is missing from the page")
+	}
+}
 
 func TestGetAddEventSourcePage(t *testing.T) {
 	req, err := http.NewRequest("GET", "/admin", nil)
