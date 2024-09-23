a43a3fa01d08dcb26afea155901732a8ee60f7c0
Brandon Tripp <brandongtripp@gmail.com>
2024-09-23 15:24:58 -0600

    fix(rds_api): change to defined name identifier for the cluster in dev and prod

--- stacks/RdsStack.ts
+++ stacks/RdsStack.ts
@@ -6,15 +6,7 @@ export function RdsStack({ stack, app }: StackContext) {
   const DATABASE = 'MeetnearmeRdsDB';
 
   // Generate a unique identifier based on the stack name and stage
-  const uniqueId = `${app.name}-${app.stage}-${stack.stackName}`.toLowerCase();
-
-  // Ensure the identifier is not longer than 63 characters, starts with a letter, and doesn't end with a hyphen
-  const clusterIdentifier = `rds-${uniqueId}`
-    .replace(/[^a-z0-9-]/g, '-') // Replace any non-alphanumeric characters with hyphens
-    .replace(/^[^a-z]/, 'rds') // Ensure it starts with a letter
-    .replace(/-+/g, '-') // Replace multiple consecutive hyphens with a single hyphen
-    .replace(/-$/, '') // Remove trailing hyphen if present
-    .slice(0, 63);
+  const clusterIdentifier = `${app.stage}-meetnearme-go-fullstack-cluster`
 
   // Create RDS cluster
   const cluster = new RDS(stack, 'Cluster', {
