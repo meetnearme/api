80fb6c480f1b73b93892d259cde22c5953834440
Brian Feister <brian.feister@gmail.com>
2024-09-24 07:48:56 -0600

    revert

--- stacks/StorageStack.ts
+++ stacks/StorageStack.ts
@@ -1,92 +1,51 @@
 import { StackContext, Table } from 'sst/constructs';
-import { DynamoDBClient, DescribeTableCommand } from '@aws-sdk/client-dynamodb';
 
-export async function StorageStack({ stack }: StackContext) {
-  const client = new DynamoDBClient({});
-
-  async function tableExists(tableName: string) {
-    try {
-      await client.send(new DescribeTableCommand({ TableName: tableName }));
-      return true;
-    } catch (error) {
-      if (error.name === 'ResourceNotFoundException') {
-        return false;
-      }
-      throw error;
-    }
-  }
-
-  const registrationsTableName = 'Registrations';
-  const registrationsTableExists = await tableExists(registrationsTableName);
-
+export function StorageStack({ stack }: StackContext) {
   // Create the `Registrations` table
-  let registrationsTable;
-  if (!registrationsTableExists) {
-    registrationsTable = new Table(stack, registrationsTableName, {
-      fields: {
-        eventId: 'string',
-        userId: 'string',
-        responses: 'string', // this is an array, no type for arrays
-        createdAt: 'number',
-        updatedAt: 'number',
-        updatedBy: 'string',
-      },
-      primaryIndex: { partitionKey: 'eventId', sortKey: 'userId' },
-    });
-  }
+  const registrationsTable = new Table(stack, 'Registrations', {
+    fields: {
+      eventId: 'string',
+      userId: 'string',
+      responses: 'string', // this is an array, no type for arrays
+      createdAt: 'number',
+      updatedAt: 'number',
+      updatedBy: 'string',
+    },
+    primaryIndex: { partitionKey: 'eventId', sortKey: 'userId' },
+  });
 
-  const registrationFieldsTableName = 'RegistrationFields';
-  const registrationFieldsTableExists = await tableExists(
-    registrationFieldsTableName,
-  );
+  const registrationFieldsTable = new Table(stack, 'RegistrationFields', {
+    fields: {
+      eventId: 'string',
+      fields: 'string', // this is an array of registrationFields
+      createdAt: 'number',
+      updatedAt: 'number',
+      updatedBy: 'string',
+    },
+    primaryIndex: { partitionKey: 'eventId' },
+  });
 
-  // Create the `Registrations` table
-  let registrationFieldsTable;
-  if (!registrationFieldsTableExists) {
-    const registrationFieldsTable = new Table(
-      stack,
-      registrationFieldsTableName,
-      {
-        fields: {
-          eventId: 'string',
-          fields: 'string', // this is an array of registrationFields
-          createdAt: 'number',
-          updatedAt: 'number',
-          updatedBy: 'string',
-        },
-        primaryIndex: { partitionKey: 'eventId' },
-      },
-    );
-  }
-
-  const SeshuSessionsTableName = 'SeshuSessions';
-  const SeshuSessionsTableExists = await tableExists(SeshuSessionsTableName);
-
-  // Create the `Registrations` table
-  let seshuSessionsTable;
-  if (!SeshuSessionsTableExists) {
-    const seshuSessionsTable = new Table(stack, SeshuSessionsTableName, {
-      fields: {
-        ownerId: 'string',
-        url: 'string',
-        urlDomain: 'string',
-        urlPath: 'string',
-        urlQueryParams: 'string',
-        locationLatitude: 'number',
-        locationLongitude: 'number',
-        locationAddress: 'string',
-        html: 'string',
-        status: 'string',
-        eventCandidates: 'string', // this is an array, but there is no type for arrays
-        eventValidations: 'string', // this is an array, but there is no type for arrays
-        expireAt: 'number',
-        createdAt: 'number',
-        updatedAt: 'number',
-      },
-      primaryIndex: { partitionKey: 'url' },
-      timeToLiveAttribute: 'expireAt',
-    });
-  }
+  const seshuSessionsTable = new Table(stack, 'SeshuSessions', {
+    fields: {
+      ownerId: 'string',
+      url: 'string',
+      urlDomain: 'string',
+      urlPath: 'string',
+      urlQueryParams: 'string',
+      locationLatitude: 'number',
+      locationLongitude: 'number',
+      locationAddress: 'string',
+      html: 'string',
+      status: 'string',
+      eventCandidates: 'string', // this is an array, but there is no type for arrays
+      eventValidations: 'string', // this is an array, but there is no type for arrays
+      expireAt: 'number',
+      createdAt: 'number',
+      updatedAt: 'number',
+    },
+    primaryIndex: { partitionKey: 'url' },
+    timeToLiveAttribute: 'expireAt',
+  });
 
   return {
     registrationsTable,
