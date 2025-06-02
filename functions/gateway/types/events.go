67e3f9a6a7c02a24500dc5114d766e36b8426975
Brandon Tripp <brandongtripp@gmail.com>
2025-06-02 07:39:34 -0600

    feat(monolith_dockerization_phase1): add interface for vector db services

--- functions/gateway/types/events.go
+++ functions/gateway/types/events.go
@@ -1,6 +1,9 @@
 package types
 
-import "time"
+import (
+	"context"
+	"time"
+)
 
 type Event struct {
 	Id                    string        `json:"id,omitempty"`
@@ -40,4 +43,12 @@ type EventSearchResponse struct {
 	Events []Event `json:"events"`
 	Filter string  `json:"filter,omitempty"`
 	Query  string  `json:"query,omitempty"`
+}
+
+type EventService interface {
+	BulkUpsertEvent(ctx context.Context, events []Event) error
+	SearchEvents(ctx context.Context, query string, userLocation []float64, maxDistance float64, startTime, endTime int64, ownerIds []string, categories string, address string, parseDates string, eventSourceTypes []string, eventSourceIds []string) (EventSearchResponse, error)
+	BulkGetEventByID(ctx context.Context, docIds []string, parseDates string) ([]*Event, error)
+	GetEventByID(ctx context.Context, docId string, parseDates string) (*Event, error)
+	BulkDeleteEvents(ctx context.Context, docIds []string) error
 }
