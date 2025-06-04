7247788f08ada7169fafaf3bae06d69af8f3c063
Brian Feister <brian.feister@gmail.com>
2025-06-04 06:24:42 -0600

    improve error handling for mnm cloudflare options put call, convert bytes to string and write to error logs

--- functions/gateway/helpers/utils.go
+++ functions/gateway/helpers/utils.go
@@ -278,7 +278,12 @@ func SetCloudflareMnmOptions(subdomainValue, userID string, metadata map[string]
 	}
 	defer resp.Body.Close()
 	if resp.StatusCode != http.StatusOK {
-		log.Printf("failed to set KV: %s: %s", resp.Status, resp.Body)
+		bodyBytes, err := io.ReadAll(resp.Body)
+		if err != nil {
+			log.Printf("failed to read error response body: %v", err)
+			return fmt.Errorf("failed to set KV: %s", resp.Status)
+		}
+		log.Printf("failed to set KV: %s: %s", resp.Status, string(bodyBytes))
 		return fmt.Errorf("failed to set KV: %s", resp.Status)
 	}
 
