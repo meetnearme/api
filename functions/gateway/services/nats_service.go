6719ab7a7db435686fc0eaf89c328b4b2470b8dc
Lim Wei Jie <stabbershade@hotmail.com>
2025-07-24 23:08:15 +0800

    <fix> Consume and peek

--- functions/gateway/services/nats_service.go
+++ functions/gateway/services/nats_service.go
@@ -15,6 +15,7 @@ import (
 var (
 	streamName  = os.Getenv("NATS_SESHU_STREAM_NAME")
 	subjectName = os.Getenv("NATS_SESHU_STREAM_SUBJECT")
+	durableName = os.Getenv("NATS_SESHU_STREAM_DURABLE_NAME")
 )
 
 type NatsService struct {
@@ -36,7 +37,7 @@ func NewNatsService(ctx context.Context, conn *nats.Conn) (*NatsService, error) 
 
 		fmt.Printf("Stream %s does not exist, creating it...\n", streamName)
 
-		_, err = js.CreateStream(ctx, jetstream.StreamConfig{ // create stream if it does not exist
+		_, err = js.CreateStream(ctx, jetstream.StreamConfig{
 			Name:     streamName,
 			Subjects: []string{subjectName},
 		})
@@ -60,54 +61,42 @@ func GetNatsClient() (*nats.Conn, error) {
 	return nats.Connect(url)
 }
 
-// func (s *NatsService) PeekTopOfQueue(ctx context.Context) (*jetstream.RawStreamMsg, error) {
-// 	stream, err := s.js.Stream(ctx, streamName)
-// 	if err != nil {
-// 		return nil, fmt.Errorf("failed to get stream: %w", err)
-// 	}
-
-// 	msg, _ := stream.GetLastMsgForSubject(ctx, streamName)
-
-// 	return msg, nil
-// }
-
 func (s *NatsService) PeekTopOfQueue(ctx context.Context) (*jetstream.RawStreamMsg, error) {
 	stream, err := s.js.Stream(ctx, streamName)
 	if err != nil {
 		return nil, fmt.Errorf("failed to get stream: %w", err)
 	}
 
-	// Use a dedicated durable for peeking
 	consumer, err := stream.CreateOrUpdateConsumer(ctx, jetstream.ConsumerConfig{
-		Durable:       "peek-durable",
+		Name:          durableName,
 		AckPolicy:     jetstream.AckExplicitPolicy,
 		DeliverPolicy: jetstream.DeliverAllPolicy,
+		FilterSubject: subjectName,
 	})
 	if err != nil {
 		return nil, fmt.Errorf("failed to create consumer: %w", err)
 	}
 
-	// Step 1: check if the queue is empty
 	info, err := consumer.Info(ctx)
 	if err != nil {
 		return nil, fmt.Errorf("failed to get consumer info: %w", err)
 	}
 
-	infoBytes, err := json.MarshalIndent(info, "", "  ")
+	nextStreamSeq := info.AckFloor.Stream + 1
+
+	streamInfo, err := stream.Info(ctx)
 	if err != nil {
-		log.Printf("Failed to marshal consumer info: %v\n", err)
-		return nil, err
+		return nil, fmt.Errorf("failed to get stream info: %w", err)
 	}
 
-	log.Printf("Consumer Info:\n%s\n", string(infoBytes))
-
-	if info.NumPending == 0 {
+	if nextStreamSeq > streamInfo.State.LastSeq {
+		log.Printf("No message at seq %d (stream ends at %d)", nextStreamSeq, streamInfo.State.LastSeq)
 		return nil, nil
 	}
 
-	msg, err := stream.GetLastMsgForSubject(ctx, subjectName)
+	msg, err := stream.GetMsg(ctx, nextStreamSeq)
 	if err != nil {
-		return nil, fmt.Errorf("failed to get last message: %w", err)
+		return nil, fmt.Errorf("failed to get stream msg at seq %d: %w", nextStreamSeq, err)
 	}
 
 	return msg, nil
@@ -120,27 +109,49 @@ func (s *NatsService) PublishMsg(ctx context.Context, job interface{}) error {
 		return fmt.Errorf("failed to marshal job: %w", err)
 	}
 
-	_, err = s.js.Publish(ctx, subjectName, data)
+	ack, err := s.js.Publish(ctx, subjectName, data)
 	if err != nil {
 		return fmt.Errorf("failed to publish message: %w", err)
 	}
+
+	fmt.Printf("Published msg with sequence number %d on stream %q\n", ack.Sequence, ack.Stream)
 
 	return nil
 }
 
 func (s *NatsService) ConsumeMsg(ctx context.Context, workers int) error {
 
-	consumer, err := s.js.CreateOrUpdateConsumer(ctx, streamName, jetstream.ConsumerConfig{
-		Name:        "consumer",
-		Durable:     "seshu-consumer",
-		Description: "Consumer for processing jobs",
-		BackOff: []time.Duration{
-			5 * time.Second,
-		},
-		FilterSubject: subjectName,
+	cons, err := s.js.CreateOrUpdateConsumer(ctx, streamName, jetstream.ConsumerConfig{
+		Durable:       durableName,
 		AckPolicy:     jetstream.AckExplicitPolicy,
+		FilterSubject: subjectName,
 	})
-	return nil
+	if err != nil {
+		return fmt.Errorf("failed to create or update consumer: %w", err)
+	}
+
+	iter, err := cons.Messages(jetstream.PullMaxMessages(1))
+	if err != nil {
+		return fmt.Errorf("failed to get iterator: %w", err)
+	}
+
+	sem := make(chan struct{}, workers)
+
+	for {
+		sem <- struct{}{}
+		go func() {
+			defer func() {
+				<-sem
+			}()
+			msg, err := iter.Next()
+			if err != nil {
+				// Check if context is cancelled
+			}
+			fmt.Printf("Processing msg: %s\n", string(msg.Data()))
+			time.Sleep(time.Second * 60) // Simulate processing time
+			msg.Ack()
+		}()
+	}
 }
 
 func (s *NatsService) Close() error {
