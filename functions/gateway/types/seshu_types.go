74d3d63827457c331d461391e89b12c233a409fc
Brian Feister <brian.feister@gmail.com>
2025-08-21 16:50:03 -0600

    Fix seshu type validation

--- functions/gateway/types/seshu_types.go
+++ functions/gateway/types/seshu_types.go
@@ -23,25 +23,25 @@ type EventBoolValid struct {
 	EventValidateTitle       bool `json:"event_title" validate:"required"`
 	EventValidateLocation    bool `json:"event_location" validate:"required"`
 	EventValidateStartTime   bool `json:"event_start_datetime" validate:"required"`
-	EventValidateEndTime     bool `json:"event_end_datetime" validate:"optional"`
+	EventValidateEndTime     bool `json:"event_end_datetime"`
 	EventValidateURL         bool `json:"event_url" validate:"required"`
-	EventValidateDescription bool `json:"event_description" validate:"optional"`
+	EventValidateDescription bool `json:"event_description"`
 }
 
 type SeshuSession struct {
 	OwnerId           string           `json:"ownerId" validate:"required"`
 	Url               string           `json:"url" validate:"required"`
 	UrlDomain         string           `json:"urlDomain" validate:"required"`
-	UrlPath           string           `json:"urlPath" validate:"optional"`
-	UrlQueryParams    url.Values       `json:"urlQueryParams" validate:"optional"`
-	LocationLatitude  float64          `json:"locationLatitude" validate:"optional"`
-	LocationLongitude float64          `json:"locationLongitude" validate:"optional"`
-	LocationAddress   string           `json:"locationAddress" validate:"optional"`
+	UrlPath           string           `json:"urlPath"`
+	UrlQueryParams    url.Values       `json:"urlQueryParams"`
+	LocationLatitude  float64          `json:"locationLatitude"`
+	LocationLongitude float64          `json:"locationLongitude"`
+	LocationAddress   string           `json:"locationAddress"`
 	Html              string           `json:"html" validate:"required"`
-	ChildId           string           `json:"childId" dynamodbav:"childId" validate:"optional"`
-	EventCandidates   []EventInfo      `json:"eventCandidates" validate:"optional"`
-	EventValidations  []EventBoolValid `json:"eventValidations" validate:"optional"`
-	Status            string           `json:"status" validate:"optional"`
+	ChildId           string           `json:"childId" dynamodbav:"childId"`
+	EventCandidates   []EventInfo      `json:"eventCandidates"`
+	EventValidations  []EventBoolValid `json:"eventValidations"`
+	Status            string           `json:"status"`
 	CreatedAt         int64            `json:"createdAt" validate:"required"`
 	UpdatedAt         int64            `json:"updatedAt" validate:"required"`
 	ExpireAt          int64            `json:"expireAt" validate:"required"`
@@ -63,56 +63,56 @@ type SeshuSessionInsert struct {
 	OwnerId           string           `json:"ownerId" dynamodbav:"ownerId" validate:"required"`
 	Url               string           `json:"url" dynamodbav:"url" validate:"required"`
 	UrlDomain         string           `json:"urlDomain" dynamodbav:"urlDomain" validate:"required"`
-	UrlPath           string           `json:"urlPath" dynamodbav:"urlPath" validate:"optional"`
-	UrlQueryParams    url.Values       `json:"urlQueryParams" dynamodbav:"urlQueryParams" validate:"optional"`
-	LocationLatitude  float64          `json:"locationLatitude" dynamodbav:"locationLatitude" validate:"optional"`
-	LocationLongitude float64          `json:"locationLongitude" dynamodbav:"locationLongitude" validate:"optional"`
-	LocationAddress   string           `json:"locationAddress" dynamodbav:"locationAddress" validate:"optional"`
+	UrlPath           string           `json:"urlPath" dynamodbav:"urlPath"`
+	UrlQueryParams    url.Values       `json:"urlQueryParams" dynamodbav:"urlQueryParams"`
+	LocationLatitude  float64          `json:"locationLatitude" dynamodbav:"locationLatitude"`
+	LocationLongitude float64          `json:"locationLongitude" dynamodbav:"locationLongitude"`
+	LocationAddress   string           `json:"locationAddress" dynamodbav:"locationAddress"`
 	Html              string           `json:"html" dynamodbav:"html" validate:"required"`
-	ChildId           string           `json:"childId" dynamodbav:"childId" validate:"optional"`
-	EventCandidates   []EventInfo      `json:"eventCandidates" dynamodbav:"eventCandidates" validate:"optional"`
-	EventValidations  []EventBoolValid `json:"eventValidations" dynamodbav:"eventValidations" validate:"optional"`
-	Status            string           `json:"status" dynamodbav:"status" validate:"optional"`
+	ChildId           string           `json:"childId" dynamodbav:"childId"`
+	EventCandidates   []EventInfo      `json:"eventCandidates" dynamodbav:"eventCandidates"`
+	EventValidations  []EventBoolValid `json:"eventValidations" dynamodbav:"eventValidations"`
+	Status            string           `json:"status" dynamodbav:"status"`
 	CreatedAt         int64            `json:"createdAt" dynamodbav:"createdAt" validate:"required"`
 	UpdatedAt         int64            `json:"updatedAt" dynamodbav:"updatedAt" validate:"required"`
 	ExpireAt          int64            `json:"expireAt" dynamodbav:"expireAt" validate:"required"`
 }
 type SeshuSessionUpdate struct {
-	OwnerId           string           `json:"ownerId" dynamodbav:"ownerId" validate:"optional"`
+	OwnerId           string           `json:"ownerId" dynamodbav:"ownerId"`
 	Url               string           `json:"url" dynamodbav:"url" validate:"required"`
-	UrlDomain         string           `json:"urlDomain" dynamodbav:"urlDomain" validate:"optional"`
-	UrlPath           string           `json:"urlPath" dynamodbav:"urlPath" validate:"optional"`
-	UrlQueryParams    url.Values       `json:"urlQueryParams" dynamodbav:"urlQueryParams" validate:"optional"`
-	LocationLatitude  float64          `json:"locationLatitude" dynamodbav:"locationLatitude" validate:"optional"`
-	LocationLongitude float64          `json:"locationLongitude" dynamodbav:"locationLongitude" validate:"optional"`
-	LocationAddress   string           `json:"locationAddress" dynamodbav:"locationAddress" validate:"optional"`
-	Html              string           `json:"html" dynamodbav:"html" validate:"optional"`
-	ChildId           string           `json:"childId" dynamodbav:"childId" validate:"optional"`
-	EventCandidates   []EventInfo      `json:"eventCandidates" dynamodbav:"eventCandidates" validate:"optional"`
-	EventValidations  []EventBoolValid `json:"eventValidations" dynamodbav:"eventValidations" validate:"optional"`
-	Status            string           `json:"status" dynamodbav:"status" validate:"optional"`
-	CreatedAt         int64            `json:"createdAt" dynamodbav:"createdAt" validate:"optional"`
-	UpdatedAt         int64            `json:"updatedAt" dynamodbav:"updatedAt" validate:"optional"`
-	ExpireAt          int64            `json:"expireAt" dynamodbav:"expireAt" validate:"optional"`
+	UrlDomain         string           `json:"urlDomain" dynamodbav:"urlDomain"`
+	UrlPath           string           `json:"urlPath" dynamodbav:"urlPath"`
+	UrlQueryParams    url.Values       `json:"urlQueryParams" dynamodbav:"urlQueryParams"`
+	LocationLatitude  float64          `json:"locationLatitude" dynamodbav:"locationLatitude"`
+	LocationLongitude float64          `json:"locationLongitude" dynamodbav:"locationLongitude"`
+	LocationAddress   string           `json:"locationAddress" dynamodbav:"locationAddress"`
+	Html              string           `json:"html" dynamodbav:"html"`
+	ChildId           string           `json:"childId" dynamodbav:"childId"`
+	EventCandidates   []EventInfo      `json:"eventCandidates" dynamodbav:"eventCandidates"`
+	EventValidations  []EventBoolValid `json:"eventValidations" dynamodbav:"eventValidations"`
+	Status            string           `json:"status" dynamodbav:"status"`
+	CreatedAt         int64            `json:"createdAt" dynamodbav:"createdAt"`
+	UpdatedAt         int64            `json:"updatedAt" dynamodbav:"updatedAt"`
+	ExpireAt          int64            `json:"expireAt" dynamodbav:"expireAt"`
 }
 
 type SeshuJob struct {
 	NormalizedUrlKey         string  `json:"normalized_url_key" validate:"required"`
-	LocationLatitude         float64 `json:"location_latitude,omitempty" validate:"required"`
-	LocationLongitude        float64 `json:"location_longitude,omitempty" validate:"required"`
-	LocationAddress          string  `json:"location_address,omitempty" validate:"required"`
-	LocationTimezone         string  `json:"location_timezone,omitempty" validate:"optional"`
+	LocationLatitude         float64 `json:"location_latitude,omitempty"`
+	LocationLongitude        float64 `json:"location_longitude,omitempty"`
+	LocationAddress          string  `json:"location_address,omitempty"`
+	LocationTimezone         string  `json:"location_timezone,omitempty"`
 	ScheduledHour            int     `json:"scheduled_hour,omitempty" validate:"required"` // Hour of the day (0-23)
 	TargetNameCSSPath        string  `json:"target_name_css_path" validate:"required"`
 	TargetLocationCSSPath    string  `json:"target_location_css_path" validate:"required"`
 	TargetStartTimeCSSPath   string  `json:"target_start_time_css_path" validate:"required"`
-	TargetEndTimeCSSPath     string  `json:"target_end_time_css_path,omitempty" validate:"optional"`
+	TargetEndTimeCSSPath     string  `json:"target_end_time_css_path,omitempty"`
 	TargetDescriptionCSSPath string  `json:"target_description_css_path,omitempty" validate:"required"`
 	TargetHrefCSSPath        string  `json:"target_href_css_path,omitempty" validate:"required"`
 	Status                   string  `json:"status" validate:"required"` // e.g. "HEALTHY", "WARNING", "FAILING"
 	LastScrapeSuccess        int64   `json:"last_scrape_success,omitempty" validate:"required"`
-	LastScrapeFailure        int64   `json:"last_scrape_failure,omitempty" validate:"required"`
-	LastScrapeFailureCount   int     `json:"last_scrape_failure_count" validate:"required"`
+	LastScrapeFailure        int64   `json:"last_scrape_failure,omitempty" validate:"gte=0"`
+	LastScrapeFailureCount   int     `json:"last_scrape_failure_count" validate:"gte=0"`
 	OwnerID                  string  `json:"owner_id" validate:"required"`
 	KnownScrapeSource        string  `json:"known_scrape_source"` // e.g. "MEETUP", "EVENTBRITE", etc.
 }
